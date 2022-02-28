25a78599cf0c8596964932badab41b6353c06251
Chrisahn75 <chris.ahona@gmail.com>
2022-02-28 14:41:05 +0100

    try to put loading

--- premiers_pas/App.js
+++ premiers_pas/App.js
@@ -1,20 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView,Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView >
+      <View style={styles.container}>
+        <View style={styles.container1}>
+          <Text style={styles.text1}>Text 1</Text>
+          <StatusBar style='auto'/>
+          <Text style={styles.text2}>Text 2</Text>
+          <Text style={styles.text3}>Text 3</Text>
+          <Image 
+              source={require("./assets/favicon.png")} 
+              style={{ width: 100, height: 100 }} />
+          <Image
+              source={{ uri: "https://www.konexio.eu/uploads/1/2/0/2/120245745/konexio-logo_1.png" }}
+              style={{ width: 100, height: 100 }} />
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'blue',
+    justifyContent:'center',
+  },
+  container1: {
+    backgroundColor: 'white',
+    margin: '60px',
+
+  },
+  text1: {
+    fontSize: '30px',
+    marginTop: '80px',
+    marginBottom:'80px',
+  },
+  text2: {
+    textAlign: 'center',
+    marginTop: '80px',
+    marginBottom:'80px',
+  },
+  text3: {
+    fontWeight: 'bold',
+    marginTop: '80px',
+    marginBottom:'80px',
   },
 });

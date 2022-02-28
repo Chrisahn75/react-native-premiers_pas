93a845f35e19143bdd314fb63de2c3b6f7b2cc54
Chrisahn75 <chris.ahona@gmail.com>
2022-02-28 16:03:23 +0100

    loading

--- premiers_pas/App.js
+++ premiers_pas/App.js
@@ -1,8 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { ScrollView,Image, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, Image, StyleSheet, Text, View, Alert, TouchableOpacity, ActivityIndicator} from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
-  return (
+  const [showLoading, setShowLoading] = useState(true);
+
+  const Press = () => {
+    Alert.alert("Alert message", [
+      {
+        text: "Cancel",
+        onPress: () => console.log("Cancel Pressed"),
+        style: "cancel",
+      },
+      { text: "OK", onPress: () => console.log("OK Pressed") },
+    ]);
+    setShowLoading(false);
+  };
+  return showLoading ? (
     <ScrollView >
       <View style={styles.container}>
         <View style={styles.container1}>
@@ -16,6 +30,33 @@ export default function App() {
           <Image
               source={{ uri: "https://www.konexio.eu/uploads/1/2/0/2/120245745/konexio-logo_1.png" }}
               style={{ width: 100, height: 100 }} />
+           
+            <TouchableOpacity style={styles.btn} onPress={Press}>
+            <Text>Press Here</Text>
+          </TouchableOpacity>
+          <ActivityIndicator></ActivityIndicator>
+        </View>
+      </View>
+    </ScrollView>
+  ) : (
+    <ScrollView >
+      <View style={styles.container}>
+        <View style={styles.container1}>
+          <Text style={styles.text1}>Text 1</Text>
+          <StatusBar style='auto'/>
+          <Text style={styles.text2}>Text 2</Text>
+          <Text style={styles.text3}>Text 3</Text>
+          <Image 
+              source={require("./assets/favicon.png")} 
+              style={{ width: 100, height: 100 }} />
+          <Image
+              source={{ uri: "https://www.konexio.eu/uploads/1/2/0/2/120245745/konexio-logo_1.png" }}
+              style={{ width: 100, height: 100 }} />
+
+          <TouchableOpacity style={styles.btn} onPress={Press}>
+          <Text>Press Here</Text>
+          </TouchableOpacity>
+          <ActivityIndicator></ActivityIndicator>
         </View>
       </View>
     </ScrollView>
@@ -30,22 +71,27 @@ const styles = StyleSheet.create({
   },
   container1: {
     backgroundColor: 'white',
-    margin: '60px',
+    margin: 60,
 
   },
   text1: {
-    fontSize: '30px',
-    marginTop: '80px',
-    marginBottom:'80px',
+    fontSize: 30,
+    marginTop: 80,
+    marginBottom:80,
   },
   text2: {
     textAlign: 'center',
-    marginTop: '80px',
-    marginBottom:'80px',
+    marginTop: 80,
+    marginBottom:80,
   },
   text3: {
     fontWeight: 'bold',
-    marginTop: '80px',
-    marginBottom:'80px',
+    marginTop: 80,
+    marginBottom:80,
   },
+  btn: {
+    backgroundColor: "#DDDDDD",
+    padding: 10,
+
+  }
 });
